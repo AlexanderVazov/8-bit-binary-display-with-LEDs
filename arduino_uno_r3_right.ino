fdb6a978113bcf4054a46c0e0803d807e7a51f4d
AlexanderVazov <111606463+AlexanderVazov@users.noreply.github.com>
2025-03-14 18:39:17 +0200

    Update arduino_uno_r3_right.ino

--- arduino_uno_r3_right.ino
+++ arduino_uno_r3_right.ino
@@ -1,83 +1,53 @@
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 
 LiquidCrystal_I2C lcd(0x20, 16, 2);
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
 
-int pins[8] = {13, 12, 11, 10, 9, 8, 7, 6};
-int plusPin = 3;
-int minusPin = 2;
-int num = 0;
-int oldNum = NULL;
+
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
+e
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   Serial.begin(9600);
-  for(int i = 0; i < 8; i++) {
-    pinMode(pins[i], OUTPUT);
-  }
-  pinMode(plusPin, INPUT_PULLUP);
-  pinMode(minusPin, INPUT_PULLUP);
   lcd.init();
   lcd.clear();
   lcd.backlight();
 }
 
-int bin[8];
-
 void loop() {
-  static int keypadNum = NULL;
-  buttonInput();
-  if(Serial.available()>0){
-  	keypadNum = Serial.parseInt();
+  char key = keypad.getKey();
+  static int num = 0;
+  static int oldNum = -1;
+  static int sendFlag = false;
+  switch(key){
+    case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
+    num = num * 10 + key - '0';
+    break;
+    case 'A': num /= 10; break;
+    case 'B': num = 0; break;
+    case 'C': case 'D': sendFlag = true; break;
   }
-  if(keypadNum != 0){
-    
-    num = keypadNum == -1 ? 0 : keypadNum;
-  }
-  Serial.print("Received: ");
-  Serial.println(keypadNum);
-  if(num != oldNum){
-    decimalToBinary(num, bin);
-    for(int i = 0; i < 8; i++) {
-      if(bin[i] == 1) {
-        digitalWrite(pins[i], HIGH);
-      } else {
-        digitalWrite(pins[i], LOW);
-      }
-      oldNum = num;
-  	}
+  num = constrain(num, 0, 255);
+  if(num!=oldNum){
     lcd.clear();
   	lcd.setCursor(0, 0);
   	lcd.print(num);
+    oldNum=num;
   }
-
+  if(sendFlag){
+    num = num == 0 ? -1 : num;
+    Serial.println(num);
+    sendFlag = false;
+  }
   delay(100);
-}
-
-void buttonInput() {
-  static unsigned long lastDebounceTimePlus = 0;
-  static unsigned long lastDebounceTimeMinus = 0;
-  const unsigned long debounceDelay = 100;
-
-  int statePlus = digitalRead(plusPin);
-  int stateMinus = digitalRead(minusPin);
-
-  unsigned long currentTime = millis();
-
-  if(statePlus == LOW && (currentTime - lastDebounceTimePlus) > debounceDelay) {
-    num++;
-    num = constrain(num, 0, 255);
-    lastDebounceTimePlus = currentTime;
-  }
-
-  if(stateMinus == LOW && (currentTime - lastDebounceTimeMinus) > debounceDelay) {
-    num--;
-    num = constrain(num, 0, 255);
-    lastDebounceTimeMinus = currentTime;
-  }
-}
-
-void decimalToBinary(int n, int binary[]) {
-  for(int i = 0; i < 8; i++) {
-    binary[7 - i] = n % 2;
-    n /= 2;
-  }
 }
